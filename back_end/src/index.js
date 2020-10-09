544efb9bede219b6717dcf8b5d237962b801cebd
Serra Canca <serracanca@gmail.com>
2020-10-09 17:19:33 -0400

    connect to test MongoDB cluster

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -1,6 +1,18 @@
+require("dotenv/config");
+
 const express = require('express');
  
 const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.TEST_CLUSTER_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }, () => {
+        console.log("connected to db");
+      }
+    );
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -9,4 +21,5 @@ app.get('/', (req, res) => {
 app.listen(3001, () =>
   console.log('Example app listening on port 3001!'),
 );
+
 
