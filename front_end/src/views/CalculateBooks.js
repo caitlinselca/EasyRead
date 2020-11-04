43668d162a26e3526cbf81707dfe6f4b4b57ae30
Kevin Salamanca <salamanca.kevin@gmail.com>
2020-11-04 16:51:41 -0500

    Updated the timing feature

--- front_end/src/views/CalculateBooks.js
+++ front_end/src/views/CalculateBooks.js
@@ -1,9 +1,14 @@
-import React, {  } from "react";
+import React, { } from 'react';
 import "./CalculateBooks.css";
 
 const CalculateBooksView = props => {
 
+  setTimeout(() => {
+    window.location.href = "/HomePage"
+  }, 1000); // 1000 = 1 second, we can change it to any specific time.
+
   return (
+    
     <div className="CalculateBooks">
       <br></br>
       <br></br>
@@ -15,8 +20,9 @@ const CalculateBooksView = props => {
         <br></br><br></br>
         <h1>If you’ve made an error or would like different suggestions, we recommend selecting your preferences again.</h1>
         <br></br>
-
+        
             <img className="spinninggear" src= {require("../static/Spinner-1s-200px-2.gif")}></img>
+        
         <br></br>
         <br></br>
         </p>
