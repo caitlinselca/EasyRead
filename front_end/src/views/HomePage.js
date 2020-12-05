0174194ad35e7597a5fb001fad775f342951865a
Kevin Salamanca <salamanca.kevin@gmail.com>
2020-12-05 14:52:30 -0500

    Updated book layout

--- front_end/src/views/HomePage.js
+++ front_end/src/views/HomePage.js
@@ -5,6 +5,7 @@ import NavigationBar from '../components/NavigationBar'
 import Container from '@material-ui/core/Container'
 import CalculateBooks from './CalculateBooks'
 
+
 class HomePage extends React.Component {
 
   constructor(props) {
@@ -28,26 +29,36 @@ class HomePage extends React.Component {
 
   }
 
-render(){
-  return !this.state.mounted ? (<CalculateBooks />) : (
-    <div className="HomePage">
-      <NavigationBar />
-      <Container maxWidth="md">
-        <div className = "root">
-          {this.state.books == null || this.state.books.length == 0 ? (<div> No books :( </div>) : this.state.books.map(book => {
-            return(
-              <div className = "books">
-                <div className="image"><img src={`http://covers.openlibrary.org/b/id/${book.cover}-M.jpg`}></img></div>
-                <div className="title">{book.title}</div>
-                <div className="author">{book.author}</div>
-              </div>
-            )
-          }) }
-        </div>
-      </Container>
-    </div>
-  );
-}
+  render(){
+    return !this.state.mounted ? (<CalculateBooks />) : (
+      <div className="HomePage">
+        <NavigationBar />
+        <Container maxWidth="md">
+          
+          <div className = "root">
+            {this.state.books == null || this.state.books.length == 0 ? (<div> No books :( </div>) : this.state.books.map(book => {
+              
+              return(
+                <div className = "books">
+                  <div className="image"><img src={`http://covers.openlibrary.org/b/id/${book.cover}-M.jpg`}></img></div>
+                  <div className="title">{book.title}</div>
+                  <div className="author">{book.author}</div>
+                </div>
+              )
+            }) }
+            
+          </div>
+          
+        </Container>
+        <div class="centerbutton">
+            <div class="pagination">
+              <button href="#">❮</button>
+              <button href="#">❯</button>
+            </div>
+          </div>
+      </div>
+    );
+  }
 };
 
 export default HomePage;
